a1426fcfaa10f722bc00976d2bc97dc8110b107d
CWKrahtz <221198@virtualwindow.co.za>
2023-11-07 15:08:37 +0200

    Test

--- server/index.js
+++ server/index.js
@@ -12,15 +12,15 @@ const authRoutes = require('./routes/auth')
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
 // //Used with React()
-// app.use(cors({
-//     origin: 'http://localhost:3000'
-// }))
+app.use(cors({
+    origin: '*'
+}))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -37,6 +37,7 @@ app.use(CommentRoutes)
 
 app.use("/api/users", userRoutes)
 app.use("/api/auth", authRoutes)
+
 app.use("/questionImages", express.static("questionImages"))
 
 app.use('/userImages', express.static("userImages"))
