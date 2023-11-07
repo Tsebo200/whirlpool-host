ab0fbfd94ffe8676db557d66becceac8b53796f0
CWKrahtz <221198@virtualwindow.co.za>
2023-11-07 15:19:25 +0200

    Re-ordered

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
-require('dotenv/config')
 const express = require('express')
-const app = express();
 const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose')
+
 const QuestionRoutes = require('./routes/questions')
 const CommentRoutes = require('./routes/comments')
 const userRoutes = require('./routes/users')
 const authRoutes = require('./routes/auth')
 
+require('dotenv/config')
+
+const app = express();
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
@@ -18,12 +20,23 @@ app.use((req, res, next) => {
 });
 
 // //Used with React()
-app.use(cors({
-    origin: '*'
-}))
+// app.use(cors({
+//     origin: '*'
+// }))
 
+// app.use("/api/users", userRoutes)
+// app.use("/api/auth", authRoutes)
+
+app.use('/questionImages', express.static('questionImages'))
+app.use('/userImages', express.static('userImages'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+
+app.use(QuestionRoutes)
+app.use(CommentRoutes)
+app.use(userRoutes)
+app.use(authRoutes)
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
@@ -32,18 +45,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
 
-app.use(QuestionRoutes)
-app.use(CommentRoutes)
-
-app.use("/api/users", userRoutes)
-app.use("/api/auth", authRoutes)
-
-app.use("/questionImages", express.static("questionImages"))
-
-app.use('/userImages', express.static("userImages"))
-
-app.use(userRoutes)
-app.use(authRoutes)
 //Connect to DB
 mongoose.connect(process.env.DB_CONNECTION, {
     useNewUrlParser: true,
