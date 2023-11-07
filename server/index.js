6f6930ce3af240e1627c2270212e7283169d5d0b
CWKrahtz <221198@virtualwindow.co.za>
2023-11-07 14:28:24 +0200

    Updated Files

--- server/index.js
+++ server/index.js
@@ -8,10 +8,18 @@ const CommentRoutes = require('./routes/comments')
 const userRoutes = require('./routes/users')
 const authRoutes = require('./routes/auth')
 
-//Used with React()
-app.use(cors({
-    origin: 'http://localhost:3000'
-}))
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+// //Used with React()
+// app.use(cors({
+//     origin: 'http://localhost:3000'
+// }))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
